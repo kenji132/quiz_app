aaced451f95c3a39b74fed7c27c64d797fdacaf6
kenji132 <ue52483@gmail.com>
2020-10-08 22:15:02 +0900

    finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import './quiz.dart';
+import './result.dart';
+
 // void main() {
 //   runApp(MyApp());
 // }
@@ -7,52 +10,80 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
-
   @override
   State<StatefulWidget> createState() {
     return _MyAppState();
   }
+}
 
-} 
-
-class _MyAppState extends State<MyApp>{
+class _MyAppState extends State<MyApp> {
+  final _questions = const [
+    {
+      'questionText': '好きな食べ物はなんですか？',
+      'answers': [
+        {'text': '黒', 'score': 10},
+        {'text': '赤', 'score': 7},
+        {'text': '青', 'score': 5},
+        {'text': '黄色', 'score': 3},
+      ],
+    },
+    {
+      'questionText': '好きな動物は何ですか？',
+      'answers': [
+        {'text': '犬', 'score': 3},
+        {'text': '猫', 'score': 5},
+        {'text': '熊', 'score': 7},
+        {'text': '獅子', 'score': 10},
+      ],
+    },
+    {
+      'questionText': '好きな食べ物はなんですか？',
+      'answers': [
+        {'text': 'ラーメン', 'score': 10},
+        {'text': '唐揚げ', 'score': 7},
+        {'text': 'パスタ', 'score': 3},
+        {'text': 'グラタン', 'score': 5},
+      ],
+    },
+  ];
   var _questionIndex = 0;
+  var _totalScore = 0;
 
-  void _answerQuestion() {
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
+  void _answerQuestion(int score) {
+    _totalScore += score;
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
     print(_questionIndex);
+    if (_questionIndex < _questions.length) {
+      print("Wa have more questions");
+    } else {
+      print("No more questions!");
+    }
   }
 
   @override
   Widget build(BuildContext context) {
+    // questions = [];
 
-    var questions = ['好きな食べ物はなんですか？', '好きな動物は何ですか？'];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Column(
-          children: [
-            Text(
-              questions[_questionIndex],
-              ),
-            RaisedButton(
-              child: Text('答え 1'),
-              onPressed: _answerQuestion,
-            ),
-            RaisedButton(
-              child: Text('答え 2'),
-              onPressed: () => print('答え2が選ばれました'),
-            ),
-            RaisedButton(
-              child: Text('答え 3'),
-              onPressed: () => print('答え3が選ばれました'),
-            ),
-          ],
-        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions)
+            : Result(_totalScore, _resetQuiz),
       ),
     );
   }
